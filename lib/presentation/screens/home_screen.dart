1e6f03d2a41b24ac4178a03d02157d86574871cc
jmpt21 <black5hole55@gmail.com>
2023-08-24 16:47:00 -0600

    Basic design for HomeScreen.

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:login_app/presentation/screens/login_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = 'home_screen';
@@ -9,11 +11,31 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
+        title: const Text('Home', style: TextStyle(fontWeight: FontWeight.w800)),
         centerTitle: true,
       ),
-      body: const Placeholder(),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text('¡Comencemos!',
+                  style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: 'Calibri')),
+              const SizedBox(height: 20),
+              FloatingActionButton(
+                onPressed: () {
+                  context.pushNamed(LoginScreen.routeName);
+                },
+                backgroundColor: Colors.cyanAccent,
+                splashColor: Colors.blueAccent,
+                tooltip: 'Ir al login',
+                child: const Icon(Icons.navigate_next_outlined),
+              )
+            ]),
+      ),
     );
   }
 }
-
